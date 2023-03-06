5202711e78111bf9bdb5b97a4d4cb1d682cb207d
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-03-07 00:19:12 +0530

    props added

--- src/components/News.js
+++ src/components/News.js
@@ -8,16 +8,16 @@ export class News extends Component {
                 <h2>DevNews - Top Headlines</h2>
                 <div className="row">
                     <div className="col md-4 my-3">
-                        <NewsItems title="Newstitle" description="newsDescription" />
+                        <NewsItems title="Newstitle" description="Some quick example text to build on the card title and make up the bulk of the card's content." />
                     </div>
                     <div className="col md-4 my-3">
-                        <NewsItems title="Newstitle" description="newsDescription" />
+                        <NewsItems title="Newstitle" description="Some quick example text to build on the card title and make up the bulk of the card's content." />
                     </div>
                     <div className="col md-4 my-3">
-                        <NewsItems title="Newstitle" description="newsDescription" />
+                        <NewsItems title="Newstitle" description="Some quick example text to build on the card title and make up the bulk of the card's content." />
                     </div>
                     <div className="col md-4 my-3">
-                        <NewsItems title="Newstitle" description="newsDescription" />
+                        <NewsItems title="Newstitle" description="Some quick example text to build on the card title and make up the bulk of the card's content." />
                     </div>
                 </div>
             </div>
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
     render() {
+        let { title, description } = this.props;
         return (
             <div>
                 <div className="card" style={{ width: "18rem" }}>
                     <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/tester-or-developer-what-suits-you-the-most.jpg" className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
                         <a href="/" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
