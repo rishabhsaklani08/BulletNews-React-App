addb973d75eaa54a12b4a7cd27e35e79103f1dd6
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-03-09 13:57:44 +0530

    default imageUrl set

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -6,10 +6,10 @@ export class NewsItems extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <img src={!imageUrl ? "https://wallpaperaccess.com/full/2879095.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
                     </div>
                 </div>
