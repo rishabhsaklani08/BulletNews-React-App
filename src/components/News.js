fa511bbb9790c96eb543d434c6c8ebf32a8443a6
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-03-18 00:18:29 +0530

    Added Next/Previous Button features to the page

--- src/components/News.js
+++ src/components/News.js
@@ -6,18 +6,46 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
 
     async componentDidMount() {
-        console.log('cmd');
-        let url = 'https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=c7c88e71edb04269b4a8480ec3003e6a'
+        let url = 'https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=c7c88e71edb04269b4a8480ec3003e6a&page=1&pageSize=20';
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
-        this.setState({ articles: parsedData.articles })
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
     }
+
+    handlePrevclick = async () => {
+        let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=c7c88e71edb04269b4a8480ec3003e6a&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextclick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=c7c88e71edb04269b4a8480ec3003e6a&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            console.log(parsedData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
     render() {
         return (
             <div className='container my-3'>
@@ -28,6 +56,10 @@ export class News extends Component {
                             <NewsItems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-danger" onClick={this.handlePrevclick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-danger" onClick={this.handleNextclick}>Next &rarr;</button>
                 </div>
             </div>
         )
